099c7e4e71f53b052000fda5aa63ba814dfb682a
Tomas Trnka <tomas@trnkatomas.eu>
2023-01-12 17:20:57 +0100

    looks almost good

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="container mx-auto">
-    <div class="fixed bg-gray-50 top-0 left-8 right-8 w-full rounded-lg py-8 shadow-xl opacity-75 ">
-      <AutoFillSuggest :selectedCountry="selectedCountry" v-on:change="[selectedCountry ,geojson] = $event"></AutoFillSuggest>
-      
+<div class="w-screen h-screen z-0">
+  <MapContainer :geojson="geojson" v-on:select="selected = $event"></MapContainer>
+</div>
+  <div class="container mx-auto z-50">
+    <div class="fixed bg-gray-50 top-0 left-8 right-8 w-full rounded-lg shadow-xl opacity-75 ">
+      <AutoFillSuggest :selectedCountry="selectedCountry" v-on:change="[selectedCountry ,geojson] = $event"></AutoFillSuggest>      
     </div>
   </div>
-  <div class="flex items-stretch min-h-screen pb-10">
-    <div class="flex-2 text-grey-darker text-center bg-blue-100 px-4 py-2 pt-50 m-2">
-      You have selected: <span class="font-semibold">{{ selectedCountry }}</span>
-    </div>
-    <div class="flex-1 text-grey-darker text-center bg-orange-200 px-4 py-2 m-2">
-      <MapContainer :geojson="geojson" v-on:select="selected = $event"></MapContainer>
-    </div>
-</div> 
 </template>
 
 
@@ -40,14 +34,15 @@ export default {
   name: 'App',
   components: {
     MapContainer,
-    AutoFillSuggest
+    AutoFillSuggest,
 
   },
   data: () => ({
         selected: undefined,
         selectedArtist: "Trnka",
-        selectedCountry: "neznam",
+        selectedCountry: "neznam",        
         dropdown: "ahoj",
+        toggler: false,
         options: [],
         geojson: {
           type: 'Feature',
